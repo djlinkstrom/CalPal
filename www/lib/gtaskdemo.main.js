942d12aec81c4fe6029ec922c7b41f65b616670a
djlinkstrom <djlinkstrom@gmail.com>
2013-07-31 18:07:19 -0400

    Updates in Webstorm

--- www/lib/gtaskdemo.main.js
+++ www/lib/gtaskdemo.main.js
@@ -142,15 +142,15 @@ function populateTaskList() {
 
 
 function goHome() {
-
-    getEmail();
+    var oAuth = liquid.helper.oauth;
+    $('#token').innerHTML = "Your Google Token is "+ oAuth.authCode;
+   // getEmail();
     $.mobile.changePage("#page-unauthorized", {
         transition : "none",
         reverse: false,
         changeHash: false
     });
-    var oAuth = liquid.helper.oauth;
-    $('#token').innerHTML = "Your Google Token is "+ oAuth.authCode;
+
 }
 
 function getEmail(){
