8d2edf2d3dfd20d1d1df09ce87005156326b3d7d
djlinkstrom <djlinkstrom@gmail.com>
2013-08-11 11:17:08 -0400

    Updates in Webstorm

--- www/lib/device_contacts.js
+++ www/lib/device_contacts.js
@@ -1,18 +1,28 @@
 
+$(document).ready(function() {
+    $("#device-contacts").click(function(event) {
+        alert("clicked");
+        findContacts();
+        //liquid.helper.oauth.authorize(authorizeWindowChange);
 
-function onSuccess(contacts) {
-    alert('Found ' + contacts.length + ' contacts.');
-};
+    });
 
-function onError(contactError) {
-    alert('onError!');
-};
+    function onSuccess(contacts) {
+        alert('Found ' + contacts.length + ' contacts.');
+    };
 
-function findContacts() {
-    alert("in contacts");
-    var options = new ContactFindOptions();
-    var fields = ["displayName", "name"];
-    navigator.contacts.find(fields,onSuccess, onError, options);
-};
+    function onError(contactError) {
+        alert('onError!');
+    };
+
+    function findContacts() {
+        alert("in contacts");
+        var options = new ContactFindOptions();
+        var fields = ["displayName", "name"];
+        navigator.contacts.find(fields,onSuccess, onError, options);
+    };
+
+
+});
 
 
