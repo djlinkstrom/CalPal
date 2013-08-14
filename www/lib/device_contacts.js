e8a127287227d940f6cbe9df9e41e7cd0526644e
djlinkstrom <djlinkstrom@gmail.com>
2013-08-13 22:44:25 -0400

    Updates in Webstorm

--- www/lib/device_contacts.js
+++ www/lib/device_contacts.js
@@ -21,7 +21,9 @@ $(document).ready(function() {
     function onSuccess(contacts) {
         alert('Found ' + contacts.length + ' contacts.');
         for (var i=0; i<contacts.length; i++) {
-            $('#contact-list').append('<li> <img src="img/default.png"> ' + contacts[i].displayName +'</li');
+            if(contacts[i].displayName!="null")  {
+                $('#contact-list').append('<li> <img src="img/default.png"> ' + contacts[i].displayName +'</li');
+            }
         }
 
         $.mobile.changePage("#contacts", {
