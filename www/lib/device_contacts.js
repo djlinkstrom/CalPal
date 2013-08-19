9c8fcb99ab0d1cfe7e54f11c110721a2f82e0c1f
djlinkstrom <djlinkstrom@gmail.com>
2013-08-18 20:11:09 -0400

    Updates in Webstorm

--- www/lib/device_contacts.js
+++ www/lib/device_contacts.js
@@ -24,11 +24,11 @@ $(document).ready(function() {
                 if(i<=5){
                     alert("displaying all");
                     alert(contacts[i].displayName + contacts[i].phoneNumbers[0].value);
-                    alert( '<a href="sms://"' + contacts[i].phoneNumbers[0].value +
-                        '"  data-role="button"> Invite via SMS');
+                    alert( '<a href="sms://' + contacts[i].phoneNumbers[0].value +
+                        '"  data-role="button" data-inline="true"> Invite via SMS </a>');
                     $('#contact-list').append('<li> <img src="img/default.png"> ' + contacts[i].displayName +
-                        '<a href="sms://"' + contacts[i].phoneNumbers[0].value +
-                        '"  data-role="button" data-inline="true"> Invite via SMS' + '</li');
+                        '<a href="sms://' + contacts[i].phoneNumbers[0].value +
+                        '"  data-role="button" data-inline="true"> Invite via SMS </a>' + '</li');
 
                 } /* else{
                     $('#contact-list').append('<li> <img src="img/default.png"> ' + contacts[i].displayName +'</li');
